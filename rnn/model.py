e868e3fd2923a64a9965da4274bf6d2e47d20610
Tianchang Li <37024342+LTCrazy@users.noreply.github.com>
2022-02-13 22:49:52 -0600

    issue for gpu training

--- rnn/model.py
+++ rnn/model.py
@@ -7,30 +7,52 @@ class RNN(nn.Module):
     LSTM model in encoder-decoder structure.
     """
 
-    def __init__(self, num_layers, num_tokens, dim_emb, num_hiddens):
+    def __init__(self, num_layers, num_tokens, dim_emb, batch_size):
         super(RNN, self).__init__()
         self.num_tokens = num_tokens
         self.num_layers = num_layers
-        self.num_hidden = num_hiddens
+        self.batch_size = batch_size
         self.embeddings = nn.Embedding(num_tokens, dim_emb)
-        self.linear1 = nn.Linear(3100, 200)
+        self.linear1 = nn.Linear(200, 100)
         self.linear2 = nn.Linear(200, 100)
+        self.activation = nn.Tanh()
+        self.h1 = torch.zeros(self.batch_size, 100)
+        self.h2 = torch.zeros(self.batch_size, 100)
         # Tie weights
         self.init_weights()
 
+        # self.num_hidden = num_hiddens
         # self.dropout = nn.Dropout(0.3)
         # self.lstm = nn.LSTM(dim_emb, num_hiddens, num_layers, dropout=0.3)
         # self.decoder = nn.Linear(num_hiddens, num_tokens)
         # self.decoder.weight = self.encoder.weight
 
-    def forward(self, input, hidden):
-        h1 = self.linear1(input)
-        h2 = self.linear2(h1)
-        logits = torch.matmul(h2, self.embeddings.weight)
+    def forward(self, input):
+        logits = torch.tensor(self.num_tokens)
+        # Process a batch of 20 samples in each time step
+        for col in range(input.shape[1]):
+            print('embedding on CUDA?', self.embeddings.is_cuda)
+            curr_input = input[:, col].long()
+            print('current input on CUDA?', curr_input.is_cuda)
+            x = self.embeddings(curr_input)    # should be size([20, 100])
+            print('current embed on CUDA?', x.is_cuda)
+            # print('shape of x:', x.shape)
+            self.h1 = self.activation(self.linear1(torch.cat((x, self.h1), dim=1)))
+            self.h2 = self.activation(self.linear2(torch.cat((self.h1, self.h2), dim=1)))   # size([20, 100])
+            # print('shape of h2:', self.h2.shape)
+            if col == 0:
+                logits = torch.matmul(self.h2, self.embeddings.weight.t())
+            else:
+                logits = torch.vstack((logits, torch.matmul(self.h2, self.embeddings.weight.t())))  # size([20, num_tokens])
+        print('shape of logits:', logits.shape)  # size([600, num_tokens])
         # x = self.dropout(x)
         # x = x.view(-1, self.num_tokens)
 
-        return F.log_softmax(logits), h1, h2
+        # Reset h1 & h2
+        self.h1 = torch.zeros(self.batch_size, 100)
+        self.h2 = torch.zeros(self.batch_size, 100)
+
+        return F.log_softmax(logits, dim=1)
 
     def init_weights(self):
         nn.init.uniform_(self.linear1.weight, -0.1, 0.1)
--- rnn/train.py
+++ rnn/train.py
@@ -13,7 +13,7 @@ parser.add_argument('--data', type=str, default='../wiki.')
 parser.add_argument('--emb', type=int, default=100)
 parser.add_argument('--num_hidden', type=int, default=100)
 parser.add_argument('--epochs', type=int, default=20)
-parser.add_argument('--lr', type=float, default=10)
+parser.add_argument('--lr', type=float, default=0.1)
 parser.add_argument('--num_layers', type=int, default=2)
 parser.add_argument('--batch_size', type=int, default=20)
 parser.add_argument('--window_size', type=int, default=30)
@@ -27,23 +27,26 @@ torch.manual_seed(args.seed)
 
 # Set the device.
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('device:', device)
 
 
 # Create a batch from the given data.
 def create_batch(data, batch_size):
     num_batch = data.size(0) // batch_size
+    # num_sample = num_batch*batch_size
     data = data.narrow(0, 0, num_batch*batch_size)
-    data = data.view(batch_size, -1).t().contiguous()
+    data = data.view(batch_size, -1).contiguous()   # --modified-- #
 
     return data.to(device)
 
 
 # Provide a batch within the source.
 def get_batch(source, i):
-    seq_len = min(args.window_size, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+seq_len+1].view(-1)
+    seq_len = min(args.window_size, len(source[0]) - 1 - i)
+    data = source[:, i:i+seq_len]
+    target = source[:, i+1:i+seq_len+1].t().reshape(-1)
 
+    # return data.to(device), target.to(device)
     return data, target
 
 
@@ -60,63 +63,96 @@ train_data = create_batch(corpus.train, args.batch_size)
 val_data = create_batch(corpus.val, args.batch_size)
 test_data = create_batch(corpus.test, args.batch_size)
 
+num_sample_train = train_data.shape[0]*train_data.shape[1]
+num_sample_val = val_data.shape[0]*val_data.shape[1]
+num_sample_test = test_data.shape[0]*test_data.shape[1]
+
+
+# Preview data
+data, targets = get_batch(train_data, 0)
+print('size of data and target:', data.shape,targets.shape)  # torch.Size([30,20]) torch.Size([600])
+# print('view data', data[1:10,:])
+# print('view target', targets[:200])
+
+
+
 # Model definition.
 num_tokens = len(corpus.encoding.index2word)
-model = model.LSTM(args.num_layers, num_tokens, args.emb, args.num_hidden).to(device)
+model = model.RNN(args.num_layers, num_tokens, args.emb, args.batch_size).to(device)
 criterion = nn.NLLLoss()
+optimizer = torch.optim.Adam(model.parameters())
 
 # Train
 def train():
     model.train()
     total_loss = 0
 
-    hidden = model.init_hidden(args.batch_size)
+    # hidden = model.init_hidden(args.batch_size)
 
     for batch, i in enumerate(range(0, train_data.size(0) - 1, args.window_size)):
         data, targets = get_batch(train_data, i)
         model.zero_grad()
 
-        hidden = clear_hidden(hidden)
-        x, hidden = model(data, hidden)
+        # hidden = clear_hidden(hidden)
+        print('model on CUDA?', next(model.parameters()).is_cuda)
+        x = model(data)
         loss = criterion(x, targets)
         loss.backward()
+        # UPDATE MODEL PARAMETERS
+        optimizer.step()
 
         # Grad clip
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.25)
-        for p in model.parameters():
-            p.data.add_(p.grad, alpha=-args.lr)
+        # torch.nn.utils.clip_grad_norm_(model.parameters(), 0.25)
+        # for p in model.parameters():
+        #     p.data.add_(p.grad, alpha=-args.lr)
+
         total_loss += loss.item()
 
         if batch % args.log_interval == 0 and batch > 0:
             tmp_loss = total_loss / args.log_interval
             print('epoch {}, loss {}, perplexity {}'.format(epoch, tmp_loss, math.exp(tmp_loss)))
             total_loss = 0
+    perplex = math.exp(total_loss / num_sample_train)
+    return perplex
 
 
 # Eval or test.
-def evaluate(data):
+def evaluate(data, test = False):
     model.eval()
     total_loss = 0.
-    hidden = model.init_hidden(args.batch_size)
+    # hidden = model.init_hidden(args.batch_size)
 
     with torch.no_grad():
         for i in range(0, data.size(0)-1, args.window_size):
             x, targets = get_batch(data, i)
-            output, hidden = model(x, hidden)
-            hidden = clear_hidden(hidden)
-            total_loss += criterion(output, targets).item() * len(x)
+            output = model(x)
+            # hidden = clear_hidden(hidden)
+            total_loss += criterion(output, targets).item()
+    if not test:
+        perplex = math.exp(total_loss / num_sample_val)
+    else:
+        perplex = math.exp(total_loss / num_sample_test)
 
-    return total_loss / (len(data) - 1)
+    return perplex
 
 
 # Training process.
+train_perplex = []
+eval_perplex = []
+test_perplex = []
+
 for epoch in range(1, args.epochs + 1):
-    train()
+    # train
+    train_perplex.append(train())
 
     # Eval
-    val_loss = evaluate(val_data)
-    print('epoch {}, val loss {}, val perplexity {}'.format(epoch, val_loss, math.exp(val_loss)))
+    eval_perplex.append(evaluate(val_data))
+    print('epoch {}, val perplexity {}'.format(epoch, eval_perplex[-1]))
 
     # Testing
-    test_loss = evaluate(test_data)
-    print('epoch {}, test loss {}, test perplexity {}'.format(epoch, test_loss, math.exp(test_loss)))
+    test_perplex.append(evaluate(test_data, test=True))
+    print('epoch {}, test perplexity {}'.format(epoch, test_perplex))
+
+# final perplexity
+print(f'final perplexities: train - {train_perplex[-1]}, eval - {eval_perplex[-1]},'
+      f' test - {test_perplex[-1]}')
